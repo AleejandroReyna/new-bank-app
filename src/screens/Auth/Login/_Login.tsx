31a09763d8571e80b764eaf86d6aac1b319e06ec
Alejandro Reyna <me@alejandroreyna.com>
2024-10-26 17:24:26 -0600

    Config: Base config (#1)
    * config: bootstrap and react bootstrap working
    
    * config: Added main navigation

---
+++ src/screens/Auth/Login/_Login.tsx
@@ -0,0 +1,9 @@
+// UI Components
+import { Link } from "react-router-dom"
+
+export const Login = () => (
+    <div>
+        <h1>Login</h1>
+        <Link to='/'>Go Home</Link>
+    </div>
+)
---
+++ src/screens/Home/_Home.tsx
@@ -0,0 +1,9 @@
+// UI Components
+import { Link } from "react-router-dom"
+
+export const Home = () => (
+    <div>
+        <h1>Home</h1>
+        <Link to='login'>Go login</Link>
+    </div>
+)
