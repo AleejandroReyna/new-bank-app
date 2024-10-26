4cb3656ba8a51f23a56be83ede8b7680c68f5820
Alejandro Reyna <me@alejandroreyna.com>
2024-10-25 19:18:04 -0600

    config: bootstrap and react bootstrap working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
+  const [componentNumber, setcomponentNumber] = useState<number>(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bootstrap testing</h1>
+      <p>testing state: {componentNumber}</p>
+      <Button onClick={() => setcomponentNumber(componentNumber + 1)}>click me</Button>
     </div>
   );
 }
