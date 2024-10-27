c1972492eab7c387d9e7557f1d523c3263a811c3
Alejandro Reyna <AleejandroReyna@gmail.com>
2024-10-26 23:16:12 -0600

    home: Added base content to home

--- src/screens/Home/_Home.tsx
+++ src/screens/Home/_Home.tsx
@@ -1,9 +1,24 @@
 // UI Components
 import { Link } from "react-router-dom"
+import { Container, Row, Col } from "react-bootstrap"
+import { CenteredTron } from "../../components/common/CenteredTron"
+import { Button } from "react-bootstrap"
 
 export const Home = () => (
-    <div>
-        <h1>Home</h1>
-        <Link to='login'>Go login</Link>
-    </div>
+    <Container>
+        <Row>
+            <Col xs lg={{ span: 6, offset: 3}}>
+                <CenteredTron
+                    title="The new way to do transactions online"
+                    content="We made your life easy">
+                    <>
+                        <Button href="#" className="me-1" size="lg" variant="light">Learn more</Button>
+                        <Link to="login">
+                            <Button  size="lg">Login</Button>
+                        </Link>
+                    </>
+                </CenteredTron>
+            </Col>
+        </Row>
+    </Container>
 )
