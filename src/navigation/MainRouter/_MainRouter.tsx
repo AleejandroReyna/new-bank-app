adbcb6206fdd61cfdf17b5cd912cc4a55409094f
Alejandro Reyna <AleejandroReyna@gmail.com>
2024-10-26 19:29:06 -0600

    public-layout: Added register and base layout

--- src/navigation/MainRouter/_MainRouter.tsx
+++ src/navigation/MainRouter/_MainRouter.tsx
@@ -1,9 +1,13 @@
 // Dependencies
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Layouts
+import { Base as PublicLayout } from "../../components/layout/public/Base";
+
 // Screens
 import { Home } from "../../screens/Home";
 import { Login } from "../../screens/Auth/Login";
+import { Register } from "../../screens/Auth/Register";
 import { Profile } from "../../screens/Profile";
 import { Edit as EditProfile } from "../../screens/Profile/Edit";
 import { List as Accounts } from "../../screens/Accounts/List";
@@ -13,8 +17,11 @@ import { New as NewAccount } from "../../screens/Accounts/New";
 export const MainRouter = () => (
     <BrowserRouter>
         <Routes>
-            <Route path="/" Component={Home} />
-            <Route path="login" Component={Login} />
+            <Route Component={PublicLayout}>
+                <Route path="/" Component={Home} />
+                <Route path="login" Component={Login} />
+                <Route path="register" Component={Register} />
+            </Route>
             <Route>
                 <Route path="profile" Component={Profile} />
                 <Route path="profile/edit" Component={EditProfile} />
