4ac86bde10186ebb0277047101a7ee756e676a3a
Alejandro Reyna <AleejandroReyna@gmail.com>
2024-10-27 14:24:35 -0600

    dashboard: Added bootstrap icons
    And added some icons on sidebar

--- src/components/layout/dashboard/Sidebar/_Sidebar.tsx
+++ src/components/layout/dashboard/Sidebar/_Sidebar.tsx
@@ -13,17 +13,17 @@ export const Sidebar = () => (
       <Nav as="nav" className="flex-column">
         <span className="sidebar-heading  px-3 mt-4 mb-1 text-muted">User</span>
         <Nav.Item>
-          <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+          <Nav.Link as={Link} to="/profile"><i className="bi bi-person"></i> Profile</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link  as={Link} to="/profile/Edit">Edit Profile</Nav.Link>
+          <Nav.Link  as={Link} to="/profile/Edit"><i className="bi bi-person-gear"></i> Edit Profile</Nav.Link>
         </Nav.Item>
         <span className="sidebar-heading  px-3 mt-4 mb-1 text-muted">Accounts</span>
         <Nav.Item>
-          <Nav.Link as={Link} to="/accounts">My Accounts</Nav.Link>
+          <Nav.Link as={Link} to="/accounts"><i className="bi bi-person-vcard"></i> My Accounts</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link  as={Link} to="/accounts/new">New Account</Nav.Link>
+          <Nav.Link  as={Link} to="/accounts/new"><i className="bi bi-plus-circle"></i> New Account</Nav.Link>
         </Nav.Item>
       </Nav>
     </aside>
