eb4bfe8444fcf35eba120178b18f13c4c2d54489
Alejandro Reyna <AleejandroReyna@gmail.com>
2024-10-26 19:28:11 -0600

    public-layout: created header component

---
+++ src/components/layout/public/Header/_Header.tsx
@@ -0,0 +1,26 @@
+// Dependencies
+import { Link } from "react-router-dom"
+
+// UI Components
+import { 
+  Container, 
+  Nav, 
+  Navbar, 
+  NavDropdown 
+} from "react-bootstrap"
+
+export const Header = () => (
+  <Navbar expand="lg" className="bg-body-tertiary">
+    <Container>
+      <Navbar.Brand as={Link} to="/">New Bank App</Navbar.Brand>
+      <Navbar.Toggle aria-controls="header-nav" />
+      <Navbar.Collapse id="header-nav" className="justify-content-end">
+          <Nav className="me-auto">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="login">Login</Nav.Link>
+            <Nav.Link as={Link} to="register">Register</Nav.Link>
+          </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+)
