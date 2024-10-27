0efd256be33f24b07925340bf63e4bd4b8ee5f4c
Alejandro Reyna <AleejandroReyna@gmail.com>
2024-10-27 16:47:11 -0600

    profile: Refactored app title component
    Added actions on profile header

--- src/components/common/AppTitle/_AppTitle.tsx
+++ src/components/common/AppTitle/_AppTitle.tsx
@@ -1,13 +1,16 @@
+// UI Components
+import { Stack } from "react-bootstrap"
+
 type AppTitleProps = {
   title: string
   children?: JSX.Element
 }
 
 export const AppTitle = ({title, children} : AppTitleProps) => (
-  <div>
+  <Stack direction="horizontal" gap={2} className="mt-3">
     <h3>{title}</h3>
-    {children &&
-      <div>{children}</div>
+      {children &&
+      <div className="ms-auto">{children}</div>
     }
-  </div>
+  </Stack>
 )
--- src/screens/Profile/_Profile.tsx
+++ src/screens/Profile/_Profile.tsx
@@ -1,1 +1,16 @@
-export const Profile = () => <h1>Profile</h1>
+// Dependencies
+import { Link } from "react-router-dom"
+
+// UI Components
+import { AppTitle } from "../../components/common/AppTitle"
+import { Button } from "react-bootstrap"
+
+export const Profile = () => (
+  <>
+  <AppTitle title="Profile">
+    <Link to='/profile/edit'>
+      <Button type="button" role="button">Edit Profile</Button>
+    </Link>
+  </AppTitle>
+  </>
+)
