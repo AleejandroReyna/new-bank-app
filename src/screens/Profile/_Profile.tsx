a55e59da1c7e7a4562f50436ef0826467cdd513d
Alejandro Reyna <AleejandroReyna@gmail.com>
2024-10-27 16:55:37 -0600

    profile: Added base profile

--- src/screens/Profile/_Profile.tsx
+++ src/screens/Profile/_Profile.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 // UI Components
 import { AppTitle } from "../../components/common/AppTitle"
-import { Button } from "react-bootstrap"
+import { Button, Container, Row, Col } from "react-bootstrap"
 
 export const Profile = () => (
   <>
@@ -12,5 +12,29 @@ export const Profile = () => (
       <Button type="button" role="button">Edit Profile</Button>
     </Link>
   </AppTitle>
+  <Container  className="mt-5">
+    <Row>
+      <Col xs="12">
+        <span>Name: </span>
+        <p>Erick Alejandro</p>
+      </Col>
+      <Col xs="12">
+        <span>Last Name: </span>
+        <p>Alvarez Reyna</p>
+      </Col>
+      <Col xs="12">
+        <span>Phone number: </span>
+        <p>53535353</p>
+      </Col>
+      <Col xs="12">
+        <span>email: </span>
+        <p><a href="mailto:me@alejandroreyna.com">me@alejandroreyna.com</a></p>
+      </Col>
+      <Col xs="12">
+        <span>Address: </span>
+        <p>Guatemala City, Guatemala</p>
+      </Col>
+    </Row>
+  </Container>
   </>
 )
