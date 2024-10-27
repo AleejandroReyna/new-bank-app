c9e1cd904b4b03084a0f418fe298ca9ef9f37325
Alejandro Reyna <AleejandroReyna@gmail.com>
2024-10-27 14:07:38 -0600

    dashboard: Added sidebar

--- src/components/layout/dashboard/Base/_Base.tsx
+++ src/components/layout/dashboard/Base/_Base.tsx
@@ -3,10 +3,21 @@ import { Outlet } from "react-router-dom"
 
 // UI Components
 import { Header } from "../Header"
+import { Container, Row, Col } from "react-bootstrap"
+import { Sidebar } from "../Sidebar"
 
 export const Base = () => (
   <div className="base-container">
     <Header />
-    <Outlet />
+    <main>
+      <Container fluid>
+        <Row>   
+          <Sidebar />     
+          <Col lg={{span: 10, offset: 2}} md={{span: 9, offset: 3}}>
+            <Outlet />
+          </Col>
+        </Row>
+      </Container>
+    </main>
   </div>
 )
