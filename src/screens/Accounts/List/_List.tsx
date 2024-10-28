ea3222242d3d150adbabb4067fe4b7b762f10eb3
Alejandro Reyna <AleejandroReyna@gmail.com>
2024-10-27 19:46:17 -0600

    account-list: created account card component
    Added props and added on list

--- src/screens/Accounts/List/_List.tsx
+++ src/screens/Accounts/List/_List.tsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom"
 
 // UI Components
 import { AppTitle } from "../../../components/common/AppTitle"
-import { Button, Container, Row, Col, Card, Badge, Stack } from "react-bootstrap"
+import { Button, Container, Row, Col } from "react-bootstrap"
+import { AccountCard } from "../../../components/common/AccountCard"
 
 export const List = () => {
   return (
@@ -15,30 +16,42 @@ export const List = () => {
       </AppTitle>
       <Container className="mt-5">
         <Row>
-          <Col lg={6}>
-            <Card>
-              <Card.Header>
-                <Stack direction="horizontal" gap={2}>
-                  <h6>Saving Account: A-000456747</h6>
-                  <Badge bg="primary" className="ms-auto">Active</Badge>
-                </Stack>
-              </Card.Header>
-              <Card.Body>
-                <h2>Q. 1,500.00</h2>
-              </Card.Body>
-              <Card.Footer>
-                <Stack direction="horizontal" gap={2}>
-                  <p>Created by: Aug 13, 2020</p>
-                  <Link to="/accounts/1"  className="ms-auto">
-                    <Button size="sm">See Details</Button>
-                  </Link>
-                </Stack>
-              </Card.Footer>
-            </Card>
+          <Col lg={6} className="mb-3">
+            <AccountCard
+              id={1}
+              accountName="A-00234567"
+              status="active"
+              currency="Q"
+              amount={1500}
+              createdDate="Aug 20, 2020" />
           </Col>
-          <Col lg={6}>card here</Col>
-          <Col lg={6}>card here</Col>
-          <Col lg={6}>card here</Col>
+          <Col lg={6}>
+          <AccountCard
+              id={1}
+              accountName="A-00234567"
+              status="canceled"
+              currency="Q"
+              amount={1500}
+              createdDate="Aug 20, 2020" />
+          </Col>
+          <Col lg={6} className="mb-3">
+            <AccountCard
+              id={1}
+              accountName="A-00234567"
+              status="active"
+              currency="Q"
+              amount={1500}
+              createdDate="Aug 20, 2020" />
+          </Col>
+          <Col lg={6}>
+          <AccountCard
+              id={1}
+              accountName="A-00234567"
+              status="canceled"
+              currency="Q"
+              amount={1500}
+              createdDate="Aug 20, 2020" />
+          </Col>
         </Row>
       </Container>
     </>
