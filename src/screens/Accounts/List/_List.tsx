cdf6c3d33ab0416c493a90af881bcf5241a48437
Alejandro Reyna <me@alejandroreyna.com>
2024-10-27 19:47:30 -0600

    Added Content to Account List (#11)
    * account-list: Added base account list screen
    
    * account-list: created account card component
    
    Added props and added on list

--- src/screens/Accounts/List/_List.tsx
+++ src/screens/Accounts/List/_List.tsx
@@ -1,1 +1,59 @@
-export const List = () => <h1>Account List</h1>
+// Dependencies
+import { Link } from "react-router-dom"
+
+// UI Components
+import { AppTitle } from "../../../components/common/AppTitle"
+import { Button, Container, Row, Col } from "react-bootstrap"
+import { AccountCard } from "../../../components/common/AccountCard"
+
+export const List = () => {
+  return (
+    <>
+      <AppTitle title="Account List">
+        <Link to="/accounts/new">
+          <Button type="button" role="button" variant="light">Create Account</Button>
+        </Link>
+      </AppTitle>
+      <Container className="mt-5">
+        <Row>
+          <Col lg={6} className="mb-3">
+            <AccountCard
+              id={1}
+              accountName="A-00234567"
+              status="active"
+              currency="Q"
+              amount={1500}
+              createdDate="Aug 20, 2020" />
+          </Col>
+          <Col lg={6}>
+          <AccountCard
+              id={1}
+              accountName="A-00234567"
+              status="canceled"
+              currency="Q"
+              amount={1500}
+              createdDate="Aug 20, 2020" />
+          </Col>
+          <Col lg={6} className="mb-3">
+            <AccountCard
+              id={1}
+              accountName="A-00234567"
+              status="active"
+              currency="Q"
+              amount={1500}
+              createdDate="Aug 20, 2020" />
+          </Col>
+          <Col lg={6}>
+          <AccountCard
+              id={1}
+              accountName="A-00234567"
+              status="canceled"
+              currency="Q"
+              amount={1500}
+              createdDate="Aug 20, 2020" />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
